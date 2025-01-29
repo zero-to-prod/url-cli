a5d7054fb63e88bbfafda5176ec6ad4aafe63925
david_smith <david_smith@sweetwater.com>
2025-01-29 14:37:20 -0500

    main Renamed `src/SrcCommand.php` to `src/Src/SrcCommand.php`.

--- src/Src/SrcCommand.php
+++ src/Src/SrcCommand.php
@@ -1,10 +1,9 @@
 <?php
 
-namespace Zerotoprod\UrlCli;
+namespace Zerotoprod\UrlCli\Src;
 
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
